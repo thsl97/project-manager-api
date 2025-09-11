8a2eca251abc77f52715399072b96e9d1200643b
Thalyson Henrique <thalyssonhsl@outlook.com>
2025-09-10 21:37:16 -0300

    feat: setup repositories

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProjectsRepositoryService } from './repositories/projects.repository.service';
+import { TasksRepositoryService } from './repositories/tasks.repository.service';
+import { UsersRepositoryService } from './repositories/users.repository.service';
 
-@Module({})
+@Module({
+  providers: [ProjectsRepositoryService, TasksRepositoryService, UsersRepositoryService]
+})
 export class DatabaseModule {}
